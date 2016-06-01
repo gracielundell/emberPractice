ee2bf4e3e74c0735870d596fdf1d847cf2e470e2
Gracie Lundell <gracielundell@gmail.com>
2016-06-01 13:55:04 -0700

    add form feature - no functionality yet

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'practice',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
-    firebase: 'https://superrentals.firebaseio.com/',
+    firebase: 'https://ember-superrentals.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
